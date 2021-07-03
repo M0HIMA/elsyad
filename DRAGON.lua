ea93f74caf8ca6d6cf832f2648a5251f50ad8766
M0HIMA <86570940+M0HIMA@users.noreply.github.com>
2021-07-03 12:25:49 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -9304,20 +9304,6 @@ send(msg.chat_id_, msg.id_," ❌ تم تعطيل الرابط")
 return false end
 end
 
-if text == 'المطور' or text == 'مطور' then
-tdcli_function ({ID = "GetUser",user_id_ = SUDO},function(arg,result) 
- 
- local msg_id = msg.id_/2097152/0.5
-local Text = [[
- مطور البوت لو حابب تتواصل معاه يقمر ◐
-]]
-keyboard = {} 
-keyboard.inline_keyboard = {{{text = '   ⁽'..result.first_name_..'₎  ',url="t.me/"..result.username_}},}
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/'..result.username_..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-end,nil)
-
-
-end
 ---------------------
 
 if text == "تفعيل صورتي" or text == 'تفعيل الصوره' then
