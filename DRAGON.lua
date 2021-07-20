64b9cba3de47b8703fbbbdc567070981ed31d182
M0HIMA <86570940+M0HIMA@users.noreply.github.com>
2021-07-20 23:41:34 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -3061,7 +3061,7 @@ keyboard.inline_keyboard = {
 {{text = 'ᏟᎻ ՏΌႮᎡᏟᎬ', url = "https://t.me/SO_ALSIYAD"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/HAM09p/3738&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..token.. /sendPhoto?chat_id=  .. msg.chat_id_ ..  &photo=https://t.me/HAM09p/3738&caption=  .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 
 if text == 'مين نصبلك' or text == 'عايزه بوت' or text == 'عايز بوت' then
